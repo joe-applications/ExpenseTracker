caeab529934f8e7fbf83e0542a67bdc035092f91
joe_application <joe_application@outlook.com>
2023-03-09 21:42:38 -0600

    credit_card_expense

--- routes.py
+++ routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 from orm import db
-from models import Product, Cart
+from models import Bill
 
 
 api = Blueprint('api', __name__)
@@ -11,13 +11,35 @@ def index():
     return {'msg': 'Endpoint - index'}
 
 
+@api.route('/get')
+def get():
+    resp = {'results': []}
+    query_result = db.session.query(Bill).filter(Bill.id == 1)
+    for row in query_result:
+        resp['results'].append(str(row))
+    return {'msg': resp}
+
+
 @api.route('/add')
-def add_product():
-    product = Product(name="Milk", price=2.50)
-    try:
-        db.session.add(product)
-        db.session.commit()
-    except Exception as e:
-        print(e)
-        return {'msg': 'Insert failed'}
-    return {'msg': 'Insert Successful!'}
+def add():
+    # '2023-03-09'
+    bill = Bill(
+        name='Car payment', 
+        type="Car", 
+        amount=324.30,
+        due_date='2023-04-05'
+    )
+    db.session.add(bill)
+    db.session.commit()
+    return {'msg': 'success'}
+
+# @api.route('/add')
+# def add_product():
+#     product = Product(name="Milk", price=2.50)
+#     try:
+#         db.session.add(product)
+#         db.session.commit()
+#     except Exception as e:
+#         print(e)
+#         return {'msg': 'Insert failed'}
+#     return {'msg': 'Insert Successful!'}
--- runserver.py
+++ runserver.py
@@ -1,6 +1,5 @@
-from day1 import app
+from ExpenseTracker import app
 
 
 if __name__=='__main__':
-    print("starting server...")
     app.run(host='0.0.0.0', port='5000')
